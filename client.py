935c80da64a4b46c713f9b78a620abb679200a09
Anshul Prasashya <prasashyaanshul@gmail.com>
2025-08-22 12:43:35 +0530

    client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client=OpenAI(
-    api_key="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"
+    api_key="#######################################kFJ4dHb8jILrNhT_u3Q-A7h3Azr2WWZikwSr0avfTyj9EYHKt5IIknjas6_OmIqrqOTxd16h1_a8A"
 
 )
 
